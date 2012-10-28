3a3affa4c630555b3e149bb101fedfa3492c994e
Buford Taylor <bufordtaylor@gmail.com>
2012-10-28 00:44:25 -0700

    really removing sanitize

--- input_mixin.py
+++ input_mixin.py
@@ -6,65 +6,6 @@ from card import get_card_by_iid, get_card_and_deck
 
 class InputMixin(object):
 
-    def sanitize(self,
-        selection,
-        persistent=False,
-        player_card=False,
-        played_card=False,
-    ):
-        try:
-            card_idx = int(selection[1:])
-            if player_card:
-                if persistent:
-                    card = self.active_player.phand[card_idx]
-                else:
-                    card = self.active_player.hand[card_idx]
-            elif played_card:
-                card = self.played_user_cards[card_idx]
-            else:
-                if persistent:
-                    card = self.phand[card_idx]
-                else:
-                    card = self.hand[card_idx]
-        except (ValueError, IndexError):
-            card = None
-            card_idx = None
-            os.system(['clear','cls'][os.name == 'nt'])
-
-        # TODO this is horrible, do something with this
-        if not card:
-            if player_card:
-                if persistent:
-                    if len(self.active_player.phand) == 0:
-                        print_red(NO_CARDS)
-                    else:
-                        print_red(INVALID_CARD % (len(self.active_player.phand) - 1))
-                else:
-                    if len(self.active_player.phand) == 0:
-                        print_red(NO_CARDS)
-                    else:
-                        print_red(INVALID_CARD % (len(self.active_player.hand) - 1))
-            elif played_card:
-                print_red(INVALID_CARD % (len(self.played_user_cards) - 1))
-            else:
-                if persistent:
-                    print_red(INVALID_CARD % (len(self.phand) - 1))
-                else:
-                    print_red(INVALID_CARD % (len(self.hand) - 1))
-
-        if selection.startswith('u'):
-            if ACTION_ACQUIRE_TO_PHAND in card.actions:
-                card.move_to = WHERE_PLAYER_PERSISTENT
-            else:
-                os.system(['clear','cls'][os.name == 'nt'])
-                print_red(INVALID_SELECTION)
-                card = None
-                card_idx = None
-
-        return card, card_idx
-
-
-
     def handle_this_or_that(self, this, that):
 
         print 'ACTIONS:', ','.join([ACTION_DICT[a] for a in self.actions])
